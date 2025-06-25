bfd00b08d7376c1cc29a182e7ca21a6ec2f6566e
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-25 12:06:45 +0500

    Initial commit: Flutter counter app with unit tests
    - Added Counter class with increment functionality
    - Implemented unit tests using Given-When-Then pattern
    - Tests verify initial count is 0 and increment functionality works correctly

---
+++ test/counter_test.dart
@@ -0,0 +1,33 @@
+import 'package:counter_unit_test_app/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Given when then pattern
+  test(
+    'Given counter class when it is instantiated then value of count should be 0',
+    () {
+      // Arrange
+      final counter = Counter();
+
+      // Act
+      final count = counter.count;
+
+      // Assert
+      expect(count, 0);
+    },
+  );
+  test(
+    "given the counter class when it is incremented then value of count should be one",
+    () {
+      // Arrange
+      final counter = Counter();
+
+      // Act
+      counter.increment();
+      final count = counter.count;
+
+      // Assert
+      expect(count, 1);
+    },
+  );
+}
