af4f97c343c75e042f0a55fddadad3cf2cfea533
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-25 13:18:53 +0500

    refactor: streamline counter test setup and remove redundant instantiation

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,12 +2,28 @@ import 'package:counter_unit_test_app/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late Counter counter;
+  // pretest like init state
+  // pretest
+  setUp(() => counter = Counter());
+  //setup > test > setup > test
+  // setUpAll(() => null);
+  // setupAll >  test > test
+
+  // post test like dispose state
+  // tearDown(() {});
+  //  test > tearDown > test > tearDown
+  // tearDownAll(() => null);
+  //  test > test > tearDownAll
+
+  // test cases
+
   group("Counter class -", () {
     test(
       'Given counter class when it is instantiated then value of count should be 0',
       () {
         // Arrange
-        final counter = Counter();
+        // final counter = Counter();
 
         // Act
         final count = counter.count;
@@ -20,7 +36,7 @@ void main() {
       "given the counter class when it is incremented then value of count should be one",
       () {
         // Arrange
-        final counter = Counter();
+        // final counter = Counter();
 
         // Act
         counter.increment();
@@ -34,7 +50,7 @@ void main() {
       "given the counter class when it is decremented then value of count should be -1",
       () {
         // Arrange
-        final counter = Counter();
+        // final counter = Counter();
 
         // Act
         counter.decrement();
